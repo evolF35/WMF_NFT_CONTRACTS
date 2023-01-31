2e0846e7c6190ea945498711d1a800fb96d44867
evolF35 <maccyair@gmail.com>
2023-01-31 11:48:54 +0400

    I think ERC721 is easier

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,7 +1,20 @@
 
 pragma solidity ^0.8.9;
 
-contract Lock {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract WMF is ERC721 {
 
 
+    constructor() ERC721("WMF", "WMF") {}
+
+    function mint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://api.wmf.art/api/v1/nft/";
+    }
+    
+    
 }
