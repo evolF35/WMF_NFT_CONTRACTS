99bcd69f023d11e903ee09b8b3aa2193be3d385d
evolF35 <maccyair@gmail.com>
2023-03-23 23:59:08 +0400

    just saving

--- contracts/TheNFTCollection.sol
+++ contracts/TheNFTCollection.sol
@@ -90,4 +90,5 @@ contract WMF_NFT is ERC721URIStorage,Ownable {
     function setBaseURI(string calldata baseURI) external onlyOwner {
         _baseTokenURI = baseURI;
     }
+
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,8 +4,8 @@ require("@nomiclabs/hardhat-web3");
 
 async function main() {
 
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy();    
+    const Lock = await hre.ethers.getContractFactory("WMF_NFT");
+    const lock = await Lock.deploy("1","1","0x10328D18901bE2278f8105D9ED8a2DbdE08e709f");    
 
     await lock.deployed();
 
