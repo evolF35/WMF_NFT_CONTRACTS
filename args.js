eb1280d84b7882dba72366ed513803f9179ce416
evolF35 <maccyair@gmail.com>
2023-04-05 06:05:46 +0400

    need to update contract

--- args.js
+++ args.js
@@ -1,5 +1,3 @@
 module.exports = [
-    "1",
-    "1",
-    "0x10328D18901bE2278f8105D9ED8a2DbdE08e709f",
+
   ];
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,16 +4,20 @@ require("@nomiclabs/hardhat-web3");
 
 async function main() {
 
+    const contractAddress = '0x7a05Fe195A30d1A72fFaA3F28bF14E33794236da'; // Replace with the actual address of the deployed contract
+
     const Lock = await hre.ethers.getContractFactory("WMF_NFT4");
-    const lock = await Lock.deploy(
-    
+    // Create an instance of the deployed contract
+    const lockInstance = await Lock.attach(contractAddress);
 
+    const uri = 'https://bafybeidbyvjkqm5biyeymlihgdmqufkz4n3igue4stenn3fvel6e5xsqxy.ipfs.nftstorage.link/?filename=AFRICANCLASSICAL.png1';
+    const tokenUris = Array(50).fill(uri);
+    // Call the `setTokenUris` function with the generated array
 
-    await lock.deployed();
-
-    console.log(
-      `deployed to ${lock.address}`
-      );
+      const tx = await lockInstance.setTokenUris(tokenUris);
+      const receipt = await tx.wait();
+      console.log('Transaction successful:', receipt.transactionHash);
+  
 }
 
 main().catch((error) => {
